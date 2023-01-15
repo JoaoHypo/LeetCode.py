b85dd7c689e01dd87f871924e1af4bfacb67159d
João "Hypo" Barboza <jguibarboza@gmail.com>
2023-01-14 21:46:47 -0300

    added few comments

--- LongestCommonPrefix+assert.py
+++ LongestCommonPrefix+assert.py
@@ -36,7 +36,7 @@ class Solution:
         min_len = min([len(s) for s in strs])
         for i in range(min_len):
             for j in range(1, len(strs)):
-                #print(strs[j][i]) debug proposes
+                #print(strs[j][i]) debug purpose
                 #print(strs[0][i])
                 if strs[j][i] != strs[0][i]:
                     return strs[0][:i]
