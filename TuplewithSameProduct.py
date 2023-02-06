154aa565629e0131a7d32ba726fb1fd52b044867
João "Hypo" Barboza <jguibarboza@gmail.com>
2023-02-05 21:15:16 -0300

    few ajusts

--- TuplewithSameProduct.py
+++ TuplewithSameProduct.py
@@ -29,12 +29,12 @@ Constraints:
 All elements in nums are distinct.
 
 '''
+from collections import defaultdict
 
-class Solution:
-    def tupleSameProduct(self, nums: List[int]) -> int:
-        cnt = defaultdict(int)
-        for i in range(1, len(nums)):
-            for j in range(i):
-                x = nums[i] * nums[j]
-                cnt[x] += 1
-        return sum(v * (v - 1) // 2 for v in cnt.values()) << 3
+def tupleSameProduct(self, nums: list[int]) -> int:
+    cnt = defaultdict(int)
+    for i in range(1, len(nums)):
+        for j in range(i):
+            x = nums[i] * nums[j]
+            cnt[x] += 1
+    return sum(v * (v - 1) // 2 for v in cnt.values()) << 3
